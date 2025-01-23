bded3bbf9655d0cf27c5b325b188efd7f9d742b8
sushil-arya <sushilarya888@gmail.com>
2025-01-23 11:37:59 +0530

    day02-instruction

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World')
-})
+});
 
-app.listen(5000)
+app.post('/hello', function (req, res) {
+  res.json({
+    message: 'Hello, World!'
+  });
+});
+
+app.listen(5000);
